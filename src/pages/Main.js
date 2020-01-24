846aa3647a6b0ce59484558dd0bacfc4e2c360d7
jacksonvfo <jacksonvfo@gmail.com>
2020-01-24 18:34:39 -0300

    input for techs and for range added

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, /*Keyboard*/ } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import {MaterialIcons} from '@expo/vector-icons';
 
-function Main({navigation}) {
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
 
     useEffect(() => {
@@ -34,23 +35,38 @@ function Main({navigation}) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -24.7059218, longitude: -53.7376684 }}>
-                <Image style={styles.avatar} source={{ uri: "https://lh3.googleusercontent.com/-OdyglwHsaNw/WGJ6LWYCSyI/AAAAAAAAq5Q/DEwciibFz_8j1apqUj32joGOGtcV-JwRQCEwYBhgL/w140-h140-p/Instasize_1226154757.jpg" }} />
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -24.7059218, longitude: -53.7376684 }}>
+                    <Image style={styles.avatar} source={{ uri: "https://lh3.googleusercontent.com/-OdyglwHsaNw/WGJ6LWYCSyI/AAAAAAAAq5Q/DEwciibFz_8j1apqUj32joGOGtcV-JwRQCEwYBhgL/w140-h140-p/Instasize_1226154757.jpg" }} />
 
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', {
-                        github_username: 'jacksonvfo'
-                    });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Jackson Oliveira</Text>
-                        <Text style={styles.devBio}>This is a Bio</Text>
-                        <Text style={styles.devTechs}>React.js, React Native, Node.js</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', {
+                            github_username: 'jacksonvfo'
+                        });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Jackson Oliveira</Text>
+                            <Text style={styles.devBio}>This is a Bio</Text>
+                            <Text style={styles.devTechs}>React.js, React Native, Node.js</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                    <TextInput 
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por Tecnologia"
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false} 
+                    />
+
+                    <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                        <MaterialIcons name="my-location" size={20} color="#FFF" />
+                    </TouchableOpacity>    
+            </View>
+        </>
     )
 }
 
@@ -83,6 +99,43 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        display: 'flex',
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        marginLeft: 15,
+        paddingLeft: 15
     }
 })
 
