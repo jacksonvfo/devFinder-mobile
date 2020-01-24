4f595dcfd06186274f0fa6656bbcdbee6a25cb46
jacksonvfo <jacksonvfo@gmail.com>
2020-01-24 19:15:42 -0300

    integration with backend

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, /*Keyboard*/ } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
-import {MaterialIcons} from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import api from '../services/api';
 
 function Main({ navigation }) {
+    const [devs, setDevs] = useState([]);
     const [currentRegion, setCurrentRegion] = useState(null);
+    const [techs, setTechs] = useState('');
 
     useEffect(() => {
         async function loadInitialPosition() {
@@ -30,41 +33,63 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, []);
 
+    async function loadDevs() {
+        const { latitude, longitude } = currentRegion;
+
+        const response = await api.get('/search', {
+            params: {
+                latitude,
+                longitude,
+                techs
+            }
+        });
+
+        setDevs(response.data.devs);
+    }
+
+    function handleRegionChanged(region) {
+        setCurrentRegion(region);
+    }
+
     if (!currentRegion) {
         return null;
     }
 
     return (
         <>
-            <MapView initialRegion={currentRegion} style={styles.map}>
-                <Marker coordinate={{ latitude: -24.7059218, longitude: -53.7376684 }}>
-                    <Image style={styles.avatar} source={{ uri: "https://lh3.googleusercontent.com/-OdyglwHsaNw/WGJ6LWYCSyI/AAAAAAAAq5Q/DEwciibFz_8j1apqUj32joGOGtcV-JwRQCEwYBhgL/w140-h140-p/Instasize_1226154757.jpg" }} />
+            <MapView onRegionChangeComplete={handleRegionChanged} initialRegion={currentRegion} style={styles.map}>
+                {devs.map(dev => (
+                    <Marker key={dev._id} coordinate={{ latitude: dev.location.coordinates[1], longitude: dev.location.coordinates[0] }}>
+                    <Image style={styles.avatar} source={{ uri: dev.avatar_url }} />
 
                     <Callout onPress={() => {
                         navigation.navigate('Profile', {
-                            github_username: 'jacksonvfo'
+                            github_username: dev.github_username
                         });
                     }}>
                         <View style={styles.callout}>
-                            <Text style={styles.devName}>Jackson Oliveira</Text>
-                            <Text style={styles.devBio}>This is a Bio</Text>
-                            <Text style={styles.devTechs}>React.js, React Native, Node.js</Text>
+                            <Text style={styles.devName}>{dev.name}</Text>
+                            <Text style={styles.devBio}>{dev.bio}</Text>
+                            <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
                         </View>
                     </Callout>
                 </Marker>
+                ))}
             </MapView>
             <View style={styles.searchForm}>
-                    <TextInput 
+                <TextInput
                     style={styles.searchInput}
                     placeholder="Buscar devs por Tecnologia"
                     placeholderTextColor="#999"
                     autoCapitalize="words"
-                    autoCorrect={false} 
-                    />
+                    autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
+                />
 
-                    <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
-                        <MaterialIcons name="my-location" size={20} color="#FFF" />
-                    </TouchableOpacity>    
+                <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
             </View>
         </>
     )
